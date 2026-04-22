451072fd37c9cacb42519caf2719aa65afbb3596
Marc Hollins <marchollins@cedarville.edu>
2026-04-22 14:16:02 -0400

    feat: add create route and test typings

---
+++ src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CreatePage() {
+  return (
+    <main>
+      <h1>Create New App</h1>
+      <p>Template selection will be added in a later task.</p>
+    </main>
+  );
+}
